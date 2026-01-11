fce61305432c57baf514c4f05c67ae2aa9c89bfd
Novtsev Dmitriy <Novtsev222@gmail.com>
2026-01-12 01:51:05 +0500

    задача 1.1.3 решена

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,32 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        Util util = new Util();
+        Util.getConnection(); // проверяем соединение с БД
+
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();// Создаем таблицу Users
+
+        userService.saveUser("Иван", "Иванов", (byte) 22);
+        userService.saveUser("Петр", "Петров", (byte) 25);
+        userService.saveUser("Семен", "Семенов", (byte) 31);
+        userService.saveUser("Степан", "Степанов", (byte) 28); // добавляем 4х users
+
+        List<User> users = userService.getAllUsers();
+        for (User user : users) {
+            System.out.println(user.getId() + ": " + user.getName() + " " + user.getLastName() + ", Age: " + user.getAge()); // Получение всех User из базы
+        }
+        userService.removeUserById(3);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+// реализуйте алгоритм здесь
     }
 }
